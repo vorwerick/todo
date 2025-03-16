16bfe6dec721afe9a58c0357d3264b85cbbba31c
ales <vorwerick@email.cz>
2025-03-16 14:04:15 +0100

    fix: koin fix

--- composeApp/src/commonMain/kotlin/cz/dzubera/todo/SharedMain.kt
+++ composeApp/src/commonMain/kotlin/cz/dzubera/todo/SharedMain.kt
@@ -4,13 +4,11 @@ import org.koin.core.logger.Level
 import org.koin.mp.KoinPlatform.startKoin
 import org.koin.mp.KoinPlatform.stopKoin
 
-object SharedMain {
+class SharedMain {
 
     fun initialize() {
         startKoin(listOf(modules), Level.DEBUG)
     }
 
-    fun dispose() {
-        stopKoin()
-    }
+
 }
